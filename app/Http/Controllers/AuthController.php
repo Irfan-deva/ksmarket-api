63f63823fc805ff7697f51db3e77e1ae36636cb0
irfan-deva <irfandeva@gmail.com>
2023-10-24 14:18:53 +0530

    added auth controller for authetication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+        $token = $user->createToken('API_TOKEN')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'API_TOKEN' => $token
+        ];
+        return response($response, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+//PUBLIC ROUTES
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/products', [ProductController::class, 'index']);
 
-Route::get('/products/', [ProductController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+//PROTECTED ROUTES
+Route::group(['middleware' => ['auth:sanctum']], function () {
 });
