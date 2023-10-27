7d3669c96420dc5a56d05f15cbd6a233afa4ed56
irfan-deva <irfandeva@gmail.com>
2023-10-27 10:10:40 +0530

    ''

--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/add', [ProductController::class, 'store']);
 Route::get('/products', [ProductController::class, 'index']);
 
-
-
 //PROTECTED ROUTES
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
