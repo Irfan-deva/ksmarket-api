942dd0ea9608ef181c0d3e07937d565a1e17d552
irfan-deva <irfandeva@gmail.com>
2023-10-28 10:40:54 +0530

    added a route to get products by their category id

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,7 +20,19 @@ class ProductController extends Controller
         ]);
     }
 
-
+    public function get_products_by_category_id($category_id)
+    {
+        return  $products = DB::table('products')->where('category_id', $category_id)
+            ->join('categories', 'products.category_id', '=', 'categories.id')
+            ->select(
+                'products.id',
+                'products.product_name',
+                'products.product_description',
+                'products.product_price',
+                'products.product_thumbnail',
+                'categories.category_name'
+            )->get();
+    }
     public function store(Request $request)
     {
         $fields = $request->validate([
@@ -31,14 +43,14 @@ class ProductController extends Controller
             'category_id' => 'required|string',
         ]);
 
-        $file = $fields['thumbnail'];
+        $file = $fields['product_thumbnail'];
         $file_name = $file->getClientOriginalName();
         $file->move('media/images/products', $file_name);
         #$file_name = $file->store('/uploads/thumbnails');
         #$thumbnail = str_replace("public/uploads/thumbnails/", '', $file_name);
 
         $product = new Product();
-        $product->thumbnail = $file_name;
+        $product->product_thumbnail = $file_name;
         $product->product_name = $fields['product_name'];
         $product->product_description = $fields['product_description'];
         $product->product_price = $fields['product_price'];
--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,14 @@ use Illuminate\Support\Facades\Route;
 //PUBLIC ROUTES
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/add', [ProductController::class, 'store']);
-Route::get('/products', [ProductController::class, 'index']);
+
 Route::post('/category/add', [CategoryController::class, 'store']);
 Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::post('/product/add', [ProductController::class, 'store']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/categories/{category_id}', [ProductController::class, 'get_products_by_category_id']);
+
 
 //PROTECTED ROUTES
 Route::group(['middleware' => ['auth:sanctum']], function () {
